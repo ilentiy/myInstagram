5f5cbad316c63d298ce00bc29099d132f50777cd
ilentiy <i.brykov9@bk.ru>
2022-10-17 10:31:51 +0300

    initial commit #2

--- myInstagram/myInstagram/ViewController.swift
+++ myInstagram/myInstagram/ViewController.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+/// VC
+final class ViewController: UIViewController {
 
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
